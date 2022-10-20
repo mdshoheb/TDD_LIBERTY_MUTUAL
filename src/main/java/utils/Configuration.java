fec7b0771b1338d7c337568b4c8c47a54ca7377f
mdshoheb <qamdshoheb@gmail.com>
2022-10-20 15:35:37 -0400

    TDD 3:35PM

--- src/main/java/utils/Configuration.java
+++ src/main/java/utils/Configuration.java
@@ -4,21 +4,21 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Configuration {
-	Properties properties;
-	
+	private Properties properties;
+
 	public String getProperty(String key) {
 		return properties.getProperty(key);
 	}
-	
+
 	public Configuration() {
 		loadProperty();
 	}
-	
+
 	public void loadProperty() {
 		properties = new Properties();
 		try {
 			properties.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
